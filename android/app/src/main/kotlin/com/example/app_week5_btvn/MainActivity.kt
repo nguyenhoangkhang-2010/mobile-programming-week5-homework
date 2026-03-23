0cbb3f05feb4f41bf170bdfef37c9ae397bed054
nguyenhoangkhang-2010 <nguyenhoangkhang.241105@gmail.com>
2026-03-23 18:48:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_week5_btvn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_week5_btvn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  String name;
+  String desc;
+  double price;
+  String image;
+
+  Product({
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.image,
+  });
+}
